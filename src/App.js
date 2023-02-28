ff47049a106c42a26ed301c2d43d56e5edec30fb
dm8262a <114957367+dm8262a@users.noreply.github.com>
2023-02-28 17:11:51 -0500

    added the Gihub repo url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Daniela Mojica and it is {""}
+        <a href="https://github.com/dm8262a/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
