563fdf41b607c6787ab0cb9fe6a30479d37336f3
dm8262a <114957367+dm8262a@users.noreply.github.com>
2023-02-28 17:45:41 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Daniela Mojica and it is {""}
-        <a href="https://github.com/dm8262a/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="contaire">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Daniela Mojica and it is {""}
+          <a
+            href="https://github.com/dm8262a/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
